d8f141d93402d27b27e34d2aa15f7036a02fadb9
Jiwoo Park <jiwoo_90@naver.com>
2023-03-23 21:34:44 +0900

    Add virtual keyword to destructor

--- include/singleton.hpp
+++ include/singleton.hpp
@@ -39,7 +39,7 @@ protected:
     Singleton(Singleton&&) noexcept = delete;
     Singleton& operator=(const Singleton&) = delete;
     Singleton& operator=(Singleton&&) noexcept = delete;
-    ~Singleton() = default;
+    virtual ~Singleton() = default;
 
 private:
     virtual void ProhibitConstructFromDerived() const = 0;
