99e36b781ca7f069695b20e50bd8a51b7989a2bd
Jiwoo Park <jiwoo_90@naver.com>
2023-11-24 18:19:33 +0900

    Print error condition

--- test/test_macro.h
+++ test/test_macro.h
@@ -1,10 +1,14 @@
 #ifndef SINGLETON_TEST_MACRO_
 #define SINGLETON_TEST_MACRO_
 
-#define CHECK(expr)    \
-    do {               \
-        if (!(expr))   \
-            return -1; \
+#include <iostream>
+
+#define CHECK(expr)                            \
+    do {                                       \
+        if (!(expr)) {                         \
+            std::cout << "Failed : " << #expr; \
+            return -1;                         \
+        }                                      \
     } while (false)
 
 #endif // SINGLETON_TEST_MACRO_
