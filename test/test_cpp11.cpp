f9e165ebae42f1d7c25f821fae8a6b39fba04d68
Jiwoo Park <jiwoo_90@naver.com>
2024-01-12 01:02:09 +0900

    Wait thread launching using latch

--- test/test_cpp11.cpp
+++ test/test_cpp11.cpp
@@ -1,4 +1,6 @@
 #include <atomic>
+#include <condition_variable>
+#include <mutex>
 #include <thread>
 #include <vector>
 
@@ -19,13 +21,42 @@ private:
     std::atomic_uint32_t count_ { 0 };
 };
 
+class Latch {
+public:
+    Latch(unsigned int count)
+        : count_ { count }
+    {
+    }
+
+    void ArriveAndWait()
+    {
+        const auto current = --count_;
+
+        if (current == 0) {
+            cv_.notify_all();
+        } else {
+            std::unique_lock<std::mutex> lock { mutex_ };
+            cv_.wait(lock, [this] { return count_ == 0; });
+        }
+    }
+
+private:
+    std::mutex mutex_;
+    std::condition_variable cv_;
+    std::atomic_uint count_;
+};
+
 int main()
 {
     const auto count = std::thread::hardware_concurrency();
+    Latch block { count };
     std::vector<std::thread> threads;
 
     for (auto i = 0u; i < count; ++i) {
-        threads.emplace_back([] { Counter::GetInstance().Add(); });
+        threads.emplace_back([&block] {
+            block.ArriveAndWait();
+            Counter::GetInstance().Add();
+        });
     }
 
     for (auto& thread : threads) {
--- test/test_cpp17.cpp
+++ test/test_cpp17.cpp
@@ -1,4 +1,6 @@
 #include <atomic>
+#include <condition_variable>
+#include <mutex>
 #include <thread>
 #include <vector>
 
@@ -7,11 +9,12 @@
 #include "test_macro.h"
 
 static std::atomic_uint32_t init { 0 };
+static std::atomic_uint32_t deinit { 0 };
 
 class Counter : public SingletonDclp<Counter> {
 public:
     Counter() { ++init; }
-    ~Counter() { --init; }
+    ~Counter() { ++deinit; }
 
     void Add() { ++count_; }
     std::uint32_t GetCount() const { return count_; }
@@ -20,13 +23,40 @@ private:
     std::atomic_uint32_t count_ { 0 };
 };
 
+class Latch {
+public:
+    Latch(unsigned int count)
+        : count_ { count }
+    {
+    }
+
+    void ArriveAndWait()
+    {
+        const auto current = --count_;
+
+        if (current == 0) {
+            cv_.notify_all();
+        } else {
+            std::unique_lock lock { mutex_ };
+            cv_.wait(lock, [this] { return count_ == 0; });
+        }
+    }
+
+private:
+    std::mutex mutex_;
+    std::condition_variable cv_;
+    std::atomic_uint count_;
+};
+
 int main()
 {
     const auto count = std::thread::hardware_concurrency();
+    Latch block { count };
     std::vector<std::thread> threads;
 
     for (auto i = 0u; i < count; ++i) {
-        threads.emplace_back([] {
+        threads.emplace_back([&block] {
+            block.ArriveAndWait();
             Counter::Construct();
             Counter::GetInstance()->Add();
         });
@@ -37,9 +67,24 @@ int main()
     }
 
     CHECK(init == 1);
+    CHECK(deinit == 0);
     CHECK(Counter::GetInstance()->GetCount() == count);
 
     Counter::Destruct();
-    CHECK(init == 0);
+
+    CHECK(init == 1);
+    CHECK(deinit == 1);
+    CHECK(Counter::GetInstance() == nullptr);
+
+    Counter::Construct();
+
+    CHECK(init == 2);
+    CHECK(deinit == 1);
+    CHECK(Counter::GetInstance()->GetCount() == 0);
+
+    Counter::Destruct();
+
+    CHECK(init == 2);
+    CHECK(deinit == 2);
     CHECK(Counter::GetInstance() == nullptr);
 }
