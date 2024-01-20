a6dead5b9381100746c588548fe420269e0efa4b
Jiwoo Park <jiwoo_90@naver.com>
2024-01-20 16:05:17 +0900

    Add memory fences

--- test/test_macro.h
+++ test/test_macro.h
@@ -1,10 +1,12 @@
 #ifndef SINGLETON_TEST_MACRO_
 #define SINGLETON_TEST_MACRO_
 
+#include <atomic>
 #include <iostream>
 
 #define CHECK_IMPL(file, line, expr)                         \
     do {                                                     \
+        std::atomic_thread_fence(std::memory_order_seq_cst); \
         if (!(expr)) {                                       \
             std::cout << "Failed : " << #expr                \
                       << " (" << file << ':' << line << ')'; \
@@ -14,6 +16,7 @@
 
 #define CHECK_EQ_IMPL(file, line, lhs, rhs)                    \
     do {                                                       \
+        std::atomic_thread_fence(std::memory_order_seq_cst);   \
         if (lhs != rhs) {                                      \
             std::cout << "Failed : " << #lhs << " == " << #rhs \
                       << " which is " << lhs << " == " << rhs  \
