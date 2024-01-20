5a5d4d7f507ea7c417911da2b23e1a158b266fc1
Jiwoo Park <jiwoo_90@naver.com>
2024-01-20 15:31:26 +0900

    Print file and line on fail

--- test/test_macro.h
+++ test/test_macro.h
@@ -3,12 +3,15 @@
 
 #include <iostream>
 
-#define CHECK(expr)                            \
-    do {                                       \
-        if (!(expr)) {                         \
-            std::cout << "Failed : " << #expr; \
-            return -1;                         \
-        }                                      \
+#define CHECK_IMPL(file, line, expr)                         \
+    do {                                                     \
+        if (!(expr)) {                                       \
+            std::cout << "Failed : " << #expr                \
+                      << " (" << file << ':' << line << ')'; \
+            return -1;                                       \
+        }                                                    \
     } while (false)
+
+#define CHECK(expr) CHECK_IMPL(__FILE__, __LINE__, expr)
 
 #endif // SINGLETON_TEST_MACRO_
